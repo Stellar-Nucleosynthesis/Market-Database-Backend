7a888d574d4f8d48d4ac3adff3b42f422c9dea50
Nazar Stepanenko <stepanenkonazar356@gmail.com>
2025-03-25 15:28:22 +0200

    Initial commit

---
+++ src/test/java/tech/zlagoda/market_database_backend/MarketDatabaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zlagoda.market_database_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDatabaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
