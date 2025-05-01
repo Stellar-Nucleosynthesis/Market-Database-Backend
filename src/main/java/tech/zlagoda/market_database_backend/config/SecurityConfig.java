1c2b9e54b5d8301ada7f4b9f4f91377b870c5e01
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-05-01 13:24:49 +0300

    Minor dependencies bug fix.

--- src/main/java/tech/zlagoda/market_database_backend/config/SecurityConfig.java
+++ src/main/java/tech/zlagoda/market_database_backend/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package tech.zlagoda.market_database_backend.config;
 
-import java.beans.Customizer;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, PasswordEncoder encoder) throws Exception {
         http
-				.cors(Customizer.withDefault())
+				.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
