998afbad373f7a9c3fc2f7beb4275d3c82bf194e
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-05-01 12:00:34 +0300

    Minor bug fix.

--- src/main/java/tech/zlagoda/market_database_backend/error_handling/SpecificInfoErrorHandler.java
+++ src/main/java/tech/zlagoda/market_database_backend/error_handling/SpecificInfoErrorHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.zlagoda.market_database_backend.controllers.CategoriesController;
+import tech.zlagoda.market_database_backend.controllers.SpecificInfoController;
 import tech.zlagoda.market_database_backend.pojos.RequestResponse;
 
-@RestControllerAdvice(assignableTypes = {CategoriesController.class})
+@RestControllerAdvice(assignableTypes = {SpecificInfoController.class})
 public class SpecificInfoErrorHandler {
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<RequestResponse> exceptionHandler(Throwable e) {
